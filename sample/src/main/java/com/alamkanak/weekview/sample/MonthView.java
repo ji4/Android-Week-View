ae1fb5ff362bfa35558961824dfc3fc53d98a207
ji4 <bornofluv@gmail.com>
2017-07-05 10:34:46 +0800

    Edit MonthView's Layout
    MonthView implements BaseActivity
    Add CalendarView

--- sample/src/main/java/com/alamkanak/weekview/sample/MonthView.java
+++ sample/src/main/java/com/alamkanak/weekview/sample/MonthView.java
@@ -3,11 +3,20 @@ package com.alamkanak.weekview.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MonthView extends AppCompatActivity {
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.List;
+
+public class MonthView extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_month_view);
+    }
+
+    @Override
+    public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+        return null;
     }
 }
