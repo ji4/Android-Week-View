e3039ca65dc572b73bb936517497ba1e9ecb5233
ji4 <bornofluv@gmail.com>
2017-07-07 10:57:00 +0800

    Write getData() to DB Class

--- sample/src/main/java/com/alamkanak/weekview/sample/BasicActivity.java
+++ sample/src/main/java/com/alamkanak/weekview/sample/BasicActivity.java
@@ -2,14 +2,13 @@ package com.alamkanak.weekview.sample;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.util.Log;
 
 import com.alamkanak.weekview.WeekViewEvent;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
-import com.alamkanak.weekview.sample.FeedReaderContract.FeedEntry;
 
 /**
  * A basic example of how to use week view library.
@@ -17,206 +16,25 @@ import com.alamkanak.weekview.sample.FeedReaderContract.FeedEntry;
  * Website: http://alamkanak.github.io
  */
 public class BasicActivity extends BaseActivity {
-
     @Override
     public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+
+        Log.d("jia", "onMonthChange()");
+
+
         // Populate the week view with some events.
         List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
 
-        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(/*getContext()*/getApplicationContext());
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-// Define a projection that specifies which columns from the database
-// you will actually use after this query.
-        String[] projection = {
-                FeedEntry._ID,
-                FeedEntry.COLUMN_NAME_TITLE,
-                FeedEntry.START_TIME,
-                FeedEntry.END_TIME,
-    };
-
-// How you want the results sorted in the resulting Cursor
-        String sortOrder =
-                FeedEntry.START_TIME/*COLUMN_NAME_UPDATED*/ + " DESC";
-
-        Cursor cursor = db.query(
-                FeedEntry.TABLE_NAME,  // The table to query
-                projection,                               // The columns to return
-                FeedEntry.COLUMN_NAME_ENTRY_ID + "=?"/*selection*/,                                // The columns for the WHERE clause
-                new String[]{"1"} /*selectionArgs*/,                            // The values for the WHERE clause
-                null,                                     // don't group the rows
-                null,                                     // don't filter by row groups
-                sortOrder                                 // The sort order
-        );
-
-        if( cursor != null && cursor.moveToFirst() ) {
-            cursor.moveToFirst();
-
-            long startTimeMillisec = cursor.getLong(
-                    cursor.getColumnIndexOrThrow(FeedEntry.START_TIME)
-            );
-            long endTimeMillisec = cursor.getLong(
-                    cursor.getColumnIndexOrThrow(FeedEntry.END_TIME)
-            );
-
-            Calendar startTime = Calendar.getInstance();
-            startTime.setTimeInMillis(startTimeMillisec);
-            Log.d("jia", "startTime: "+String.valueOf(startTime));
-
-            Calendar endTime = Calendar.getInstance();
-            endTime.setTimeInMillis(endTimeMillisec);
-            Log.d("jia", "endTime: "+String.valueOf(endTime));
-
-            WeekViewEvent event = new WeekViewEvent(1, getEventTitle(startTime), startTime, endTime);
+        //Get saved data from DB
+        EventData eventData = DB.getData(getApplicationContext());
+        if(eventData != null) {
+            WeekViewEvent event = new WeekViewEvent(1, getEventTitle(eventData.startTime), eventData.startTime, eventData.endTime);
             event.setColor(getResources().getColor(R.color.event_color_01));
             events.add(event);
-
-
-            cursor.close();
-
         }
 
-
-//        Calendar startTime = Calendar.getInstance();
-////        startTime.set(Calendar.HOUR_OF_DAY, 3);
-////        startTime.set(Calendar.MINUTE, 0);
-////        startTime.set(Calendar.MONTH, newMonth - 1);
-////        startTime.set(Calendar.YEAR, newYear);
-//        Calendar endTime = (Calendar) startTime.clone();
-////        endTime.add(Calendar.HOUR, 1);
-////        endTime.set(Calendar.MONTH, newMonth - 1);
-//        WeekViewEvent event = new WeekViewEvent(1, getEventTitle(startTime), startTime, endTime);
-////        event.setColor(getResources().getColor(R.color.event_color_01));
-////        events.add(event);
-//
-//        startTime = Calendar.getInstance();
-//        startTime.set(Calendar.HOUR_OF_DAY, 3);
-//        startTime.set(Calendar.MINUTE, 30);
-//        startTime.set(Calendar.MONTH, newMonth-1);
-//        startTime.set(Calendar.YEAR, newYear);
-//        endTime = (Calendar) startTime.clone();
-//        endTime.set(Calendar.HOUR_OF_DAY, 4);
-//        endTime.set(Calendar.MINUTE, 30);
-//        endTime.set(Calendar.MONTH, newMonth-1);
-//        event = new WeekViewEvent(10, getEventTitle(startTime), startTime, endTime);
-//        event.setColor(getResources().getColor(R.color.event_color_02));
-//        events.add(event);
-//
-//        startTime = Calendar.getInstance();
-//        startTime.set(Calendar.HOUR_OF_DAY, 4);
-//        startTime.set(Calendar.MINUTE, 20);
-//        startTime.set(Calendar.MONTH, newMonth-1);
-//        startTime.set(Calendar.YEAR, newYear);
-//        endTime = (Calendar) startTime.clone();
-//        endTime.set(Calendar.HOUR_OF_DAY, 5);
-//        endTime.set(Calendar.MINUTE, 0);
-//        event = new WeekViewEvent(10, getEventTitle(startTime), startTime, endTime);
-//        event.setColor(getResources().getColor(R.color.event_color_03));
-//        events.add(event);
-//
-//        startTime = Calendar.getInstance();
-//        startTime.set(Calendar.HOUR_OF_DAY, 5);
-//        startTime.set(Calendar.MINUTE, 30);
-//        startTime.set(Calendar.MONTH, newMonth-1);
-//        startTime.set(Calendar.YEAR, newYear);
-//        endTime = (Calendar) startTime.clone();
-//        endTime.add(Calendar.HOUR_OF_DAY, 2);
-//        endTime.set(Calendar.MONTH, newMonth-1);
-//        event = new WeekViewEvent(2, getEventTitle(startTime), startTime, endTime);
-//        event.setColor(getResources().getColor(R.color.event_color_02));
-//        events.add(event);
-//
-//        startTime = Calendar.getInstance();
-//        startTime.set(Calendar.HOUR_OF_DAY, 5);
-//        startTime.set(Calendar.MINUTE, 0);
-//        startTime.set(Calendar.MONTH, newMonth - 1);
-//        startTime.set(Calendar.YEAR, newYear);
-//        startTime.add(Calendar.DATE, 1);
-//        endTime = (Calendar) startTime.clone();
-//        endTime.add(Calendar.HOUR_OF_DAY, 3);
-//        endTime.set(Calendar.MONTH, newMonth - 1);
-//        event = new WeekViewEvent(3, getEventTitle(startTime), startTime, endTime);
-//        event.setColor(getResources().getColor(R.color.event_color_03));
-//        events.add(event);
-//
-//        startTime = Calendar.getInstance();
-//        startTime.set(Calendar.DAY_OF_MONTH, 15);
-//        startTime.set(Calendar.HOUR_OF_DAY, 3);
-//        startTime.set(Calendar.MINUTE, 0);
-//        startTime.set(Calendar.MONTH, newMonth-1);
-//        startTime.set(Calendar.YEAR, newYear);
-//        endTime = (Calendar) startTime.clone();
-//        endTime.add(Calendar.HOUR_OF_DAY, 3);
-//        event = new WeekViewEvent(4, getEventTitle(startTime), startTime, endTime);
-//        event.setColor(getResources().getColor(R.color.event_color_04));
-//        events.add(event);
-//
-//        startTime = Calendar.getInstance();
-//        startTime.set(Calendar.DAY_OF_MONTH, 1);
-//        startTime.set(Calendar.HOUR_OF_DAY, 3);
-//        startTime.set(Calendar.MINUTE, 0);
-//        startTime.set(Calendar.MONTH, newMonth-1);
-//        startTime.set(Calendar.YEAR, newYear);
-//        endTime = (Calendar) startTime.clone();
-//        endTime.add(Calendar.HOUR_OF_DAY, 3);
-//        event = new WeekViewEvent(5, getEventTitle(startTime), startTime, endTime);
-//        event.setColor(getResources().getColor(R.color.event_color_01));
-//        events.add(event);
-//
-//        startTime = Calendar.getInstance();
-//        startTime.set(Calendar.DAY_OF_MONTH, startTime.getActualMaximum(Calendar.DAY_OF_MONTH));
-//        startTime.set(Calendar.HOUR_OF_DAY, 15);
-//        startTime.set(Calendar.MINUTE, 0);
-//        startTime.set(Calendar.MONTH, newMonth-1);
-//        startTime.set(Calendar.YEAR, newYear);
-//        endTime = (Calendar) startTime.clone();
-//        endTime.add(Calendar.HOUR_OF_DAY, 3);
-//        event = new WeekViewEvent(5, getEventTitle(startTime), startTime, endTime);
-//        event.setColor(getResources().getColor(R.color.event_color_02));
-//        events.add(event);
-//
-//        //AllDay event
-//        startTime = Calendar.getInstance();
-//        startTime.set(Calendar.HOUR_OF_DAY, 0);
-//        startTime.set(Calendar.MINUTE, 0);
-//        startTime.set(Calendar.MONTH, newMonth-1);
-//        startTime.set(Calendar.YEAR, newYear);
-//        endTime = (Calendar) startTime.clone();
-//        endTime.add(Calendar.HOUR_OF_DAY, 23);
-//        event = new WeekViewEvent(7, getEventTitle(startTime),null, startTime, endTime, true);
-//        event.setColor(getResources().getColor(R.color.event_color_04));
-//        events.add(event);
-//        events.add(event);
-//
-//        startTime = Calendar.getInstance();
-//        startTime.set(Calendar.DAY_OF_MONTH, 8);
-//        startTime.set(Calendar.HOUR_OF_DAY, 2);
-//        startTime.set(Calendar.MINUTE, 0);
-//        startTime.set(Calendar.MONTH, newMonth-1);
-//        startTime.set(Calendar.YEAR, newYear);
-//        endTime = (Calendar) startTime.clone();
-//        endTime.set(Calendar.DAY_OF_MONTH, 10);
-//        endTime.set(Calendar.HOUR_OF_DAY, 23);
-//        event = new WeekViewEvent(8, getEventTitle(startTime),null, startTime, endTime, true);
-//        event.setColor(getResources().getColor(R.color.event_color_03));
-//        events.add(event);
-//
-//        // All day event until 00:00 next day
-//        startTime = Calendar.getInstance();
-//        startTime.set(Calendar.DAY_OF_MONTH, 10);
-//        startTime.set(Calendar.HOUR_OF_DAY, 0);
-//        startTime.set(Calendar.MINUTE, 0);
-//        startTime.set(Calendar.SECOND, 0);
-//        startTime.set(Calendar.MILLISECOND, 0);
-//        startTime.set(Calendar.MONTH, newMonth-1);
-//        startTime.set(Calendar.YEAR, newYear);
-//        endTime = (Calendar) startTime.clone();
-//        endTime.set(Calendar.DAY_OF_MONTH, 11);
-//        event = new WeekViewEvent(8, getEventTitle(startTime), null, startTime, endTime, true);
-//        event.setColor(getResources().getColor(R.color.event_color_01));
-//        events.add(event);
-
         return events;
+
     }
 
 }
--- sample/src/main/java/com/alamkanak/weekview/sample/DB.java
+++ sample/src/main/java/com/alamkanak/weekview/sample/DB.java
@@ -43,8 +43,77 @@ public class DB extends BaseActivity{
                 values);
 
     }
+    public static EventData getData(Context context) {
+        FeedReaderDbHelper mDbHelper = new FeedReaderDbHelper(context);
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        EventData eventData = null;
+
+        // Define a projection that specifies which columns from the database
+        // you will actually use after this query.
+        String[] projection = {
+                FeedEntry._ID,
+                FeedEntry.COLUMN_NAME_TITLE,
+                FeedEntry.START_TIME,
+                FeedEntry.END_TIME,
+        };
+
+        // How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                FeedEntry.START_TIME/*COLUMN_NAME_UPDATED*/ + " DESC";
+
+        Cursor cursor = db.query(
+                FeedEntry.TABLE_NAME,  // The table to query
+                projection,                               // The columns to return
+                FeedEntry.COLUMN_NAME_ENTRY_ID + "=?"/*selection*/,                                // The columns for the WHERE clause
+                new String[]{"1"} /*selectionArgs*/,                            // The values for the WHERE clause
+                null,                                     // don't group the rows
+                null,                                     // don't filter by row groups
+                sortOrder                                 // The sort order
+        );
+
+        if (cursor != null && cursor.moveToFirst()) {
+            cursor.moveToFirst();
+
+            long startTimeMillisec = cursor.getLong(
+                    cursor.getColumnIndexOrThrow(FeedEntry.START_TIME)
+            );
+            long endTimeMillisec = cursor.getLong(
+                    cursor.getColumnIndexOrThrow(FeedEntry.END_TIME)
+            );
+
+            Calendar startTime = Calendar.getInstance();
+            startTime.setTimeInMillis(startTimeMillisec);
+            Log.d("jia", "startTime: " + String.valueOf(startTime));
+
+            Calendar endTime = Calendar.getInstance();
+            endTime.setTimeInMillis(endTimeMillisec);
+            Log.d("jia", "endTime: " + String.valueOf(endTime));
+
+            cursor.close();
+
+            eventData = new EventData(startTime, endTime);
+
+        }
+        if(eventData!=null)
+           return eventData.getEventData();
+        return null;
+    }
     @Override
     public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
         return null;
     }
+}
+class EventData{
+    Calendar startTime, endTime;
+
+    public EventData(Calendar startTime, Calendar endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    EventData getEventData(){
+        EventData eventData = new EventData(startTime, endTime);
+        return eventData;
+    }
+
 }
